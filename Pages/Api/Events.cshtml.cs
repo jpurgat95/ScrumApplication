4ab38396459b1c5f139db92a7f50a0bfcc68fcf0
jpurgat95 <j.purgat95@gmail.com>
2025-08-13 18:48:18 +0200

    Dodano funkcjonalność oraz wstępnie skonfigurowano FullCalendar JS dla wydarzeń

---
+++ Pages/Api/Events.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ScrumApplication.Data; // namespace Twojego DbContext
+using System.Linq;
+
+namespace ScrumApplication.Pages.Api;
+
+public class EventsModel : PageModel
+{
+    private readonly ScrumDbContext _context;
+
+    public EventsModel(ScrumDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult OnGet()
+    {
+        var events = _context.Events
+            .Select(e => new
+            {
+                id = e.Id,
+                title = e.Title,
+                description = e.Description,
+                start = e.StartDate.ToString("yyyy-MM-ddTHH:mm:ss"),
+                end = e.EndDate.ToString("yyyy-MM-ddTHH:mm:ss"),
+                isDone = e.IsDone
+            })
+            .ToList();
+
+        return new JsonResult(events);
+    }
+    public IActionResult OnPostToggleDone(int id)
+    {
+        var ev = _context.Events.FirstOrDefault(e => e.Id == id);
+        if (ev != null)
+        {
+            ev.IsDone = !ev.IsDone;
+            _context.SaveChanges();
+            return new JsonResult(new { success = true, isDone = ev.IsDone });
+        }
+        return new JsonResult(new { success = false });
+    }
+}
